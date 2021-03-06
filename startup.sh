39b2f8385ad3c12682770d3a516242c0958ee1a0
lim <maxeasy2@gmail.com>
2021-03-06 23:17:04 +0900

    startup.sh modify

--- startup.sh
+++ startup.sh
@@ -6,9 +6,9 @@ java -jar ${HEAP_XMS:-"-Xms256m"} ${HEAP_XMX:-"-Xmx256m"} \
     -Dfile.encoding=UTF-8 \
 	  -Dlogdir=/webapp/logs \
 	  -Dspring.profiles.active=${PROFILE:-prod} \
-	  -Dinterfaces.notification.telegram.bottokenkey=${TELEGRAM.BOTTOKEN_KEY} \
-	  -Dinterfaces.notification.telegram.bottokenvalue=${TELEGRAM.BOTTOKEN_VALUE} \
-	  -Dinterfaces.notification.telegram.chatid=${TELEGRAM.CHAT_ID} \
+	  -Dinterfaces.notification.telegram.bottokenkey=${TELEGRAM_BOTTOKEN_KEY} \
+	  -Dinterfaces.notification.telegram.bottokenvalue=${TELEGRAM_BOTTOKEN_VALUE} \
+	  -Dinterfaces.notification.telegram.chatid=${TELEGRAM_CHAT_ID} \
     -Dspring.datasource.hikari.username=${DATABASE_USERNAME} \
     -Dspring.datasource.hikari.password=${DATABASE_PASSWORD} \
     -Ddatabase.host=${DATABASE_HOST} \
