ab0794ca4151fb2ad1f354f2648c179c671af5d7
lim <maxeasy2@gmail.com>
2021-03-17 00:08:48 +0900

    공시 메세지 모웹 url 추가

--- src/main/java/net/mirapps/upbitbot/backend/api/disclosure/DisclosureController.java
+++ src/main/java/net/mirapps/upbitbot/backend/api/disclosure/DisclosureController.java
@@ -6,6 +6,7 @@ import net.mirapps.upbitbot.backend.interfaces.notification.NotificationService;
 import net.mirapps.upbitbot.backend.interfaces.upbit.disclosure.dto.DisclosureDataPostDto;
 import net.mirapps.upbitbot.backend.interfaces.upbit.disclosure.dto.DisclosureDto;
 import net.mirapps.upbitbot.backend.interfaces.upbit.disclosure.service.DisclosureService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,9 @@ public class DisclosureController {
 
     private final NotificationService notificationService;
 
+    @Value("${interfaces.upbit-web.exchange}")
+    private String upbitWebExchange;
+
     @GetMapping("/api/v1/notification/disclosure")
     public ResponseEntity<ResponseDto> notifyDisclosure() throws Exception {
         final DisclosureDto disclosureInfo = disclosureService.getDisclosureInfo();
@@ -32,12 +36,21 @@ public class DisclosureController {
                 for (DisclosureDataPostDto disclosureDataPostDto : posts) {
                     List<String> marketNameList = disclosureDataPostDto.getMarketNameList();
                     sb.append(disclosureDataPostDto.getAssets());
+                    String marketFirst = "";
                     if (marketNameList != null && marketNameList.size() > 0){
                         sb.append("(").append(
                                 String.join(",", marketNameList)
                                 .replaceAll("-" + disclosureDataPostDto.getAssets(), "")).append(")");
+                        marketFirst = marketNameList.get(0);
                     }
-                    sb.append(" : ").append(disclosureDataPostDto.getText()).append("\n\n");
+
+                    sb.append(" : ").append(disclosureDataPostDto.getText()).append("\n");
+
+                    if (!"".equals(marketFirst)) {
+                        sb.append(upbitWebExchange).append(marketFirst).append("\n");
+                    }
+
+                    sb.append("\n");
                     //sb.append(disclosureDataPostDto.getUrl()).append("\n\n");
                 }
                 notificationService.send(sb.toString());
